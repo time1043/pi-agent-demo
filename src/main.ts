6dd44566c2a3478ed14a376872945dfc218ee589
oswin-m4a <132178516+time1043@users.noreply.github.com>
2026-09-05 18:11:12 +0800

    chore: invoke model of `~/.pi/agent/settings.json`

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
-console.log(process.env.DEEPSEEK_API_KEY);
+import {
+  createAgentSession,
+  ModelRuntime,
+  SessionManager,
+} from "@earendil-works/pi-coding-agent";
+
+// ~/.pi/agent/settings.json
+const modelRuntime = await ModelRuntime.create();
+
+// createAgentSession(options?: CreateAgentSessionOptions): Promise<CreateAgentSessionResult>
+const { session } = await createAgentSession({
+  sessionManager: SessionManager.inMemory(),
+  modelRuntime,
+});
+
+session.subscribe((event) => {
+  if (
+    event.type === "message_update" &&
+    event.assistantMessageEvent.type === "text_delta"
+  ) {
+    process.stdout.write(event.assistantMessageEvent.delta);
+  }
+});
+
+await session.prompt("What files are in the current directory?");
